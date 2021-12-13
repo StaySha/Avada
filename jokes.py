3697de672e8e280998c2247f8164551619676d00
StaySha <diestod@gmail.com>
2021-12-13 13:56:47 +0200

    new version

--- jokes.py
+++ jokes.py
@@ -7,11 +7,10 @@ class Jokes():
         self.good_jokes = 0
         self.bad_jokes = 0
         self.cod = 0
-        self.saves = 0
 
     def site(self):
         with open('jokes.txt', 'w') as f:
-            for i in range(1, 6):
+            for i in range(1, 20):
                 res = requests.get('https://tproger.ru/wp-content/plugins/citation-widget/get-quote.php')
                 joke = res.text
                 f.write(f'{joke}\n')
@@ -19,18 +18,19 @@ class Jokes():
     def count_jokes(self):
         with open('jokes.txt', 'r') as f:
             for line in f:
-                if len(line) > 60:
+                if 'это' in line or len(line) >= 60:
                     with open('saved_jokes.txt', 'w') as saves:
                         saves.write(f'{line}\n')
                     self.good_jokes += 1
-                elif len(line) < 60:
-                    self.bad_jokes += 1
-                elif 'код' in line:
-                    self.cod += 1
+                else:
+                    self.bad_jokes +=1
+                # elif len(line) < 60:
+                #     self.bad_jokes += 1
 
-    def cointing(self):
+
+    def counting(self):
         print(f'Количество запросов: {self.good_jokes + self.bad_jokes + self.cod}')
-        print(f'Количество сохраненных шуток: {self.saves + self.good_jokes}')
+        print(f'Количество сохраненных шуток: {self.good_jokes}')
         print(f'Количество удачных шуток: {self.good_jokes}')
         print(f'Количество неудачных шуток: {self.bad_jokes}')
 
@@ -39,4 +39,4 @@ jokes = Jokes()
 
 jokes.site()
 jokes.count_jokes()
-jokes.cointing()
+jokes.counting()
